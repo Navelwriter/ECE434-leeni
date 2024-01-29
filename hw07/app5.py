65c03d2706a468249ad73c21018e64d828ce6738
Noah Lee <leeni@rose-hulman.edu>
2024-01-29 01:38:53 +0000

    more progress

--- hw07/app5.py
+++ hw07/app5.py
@@ -3,52 +3,50 @@
 import gpiod
 import sys
 from flask import Flask, render_template, request
+import os
 app = Flask(__name__)
 path = "/sys/class/hwmon/"
-MAX0 = path + "hwmon0/device/temperature"
-MAX1 = path + "hwmon1/device/temperature"
-MAX2 = path + "hwmon2/device/temperature"
+MAX0 = os.path.join(path, "hwmon0/device/temperature")
+MAX1 = os.path.join(path, "hwmon1/device/temperature")
+MAX2 = os.path.join(path, "hwmon2/device/temperature")
 
-W1 = [MAX0,MAX1,MAX2]
-#define sensors GPIOs
-getoffsets=[14] # P8_16
-#define actuators GPIOs
-setoffests=[18] # P9_14
-#initialize GPIO status variables
-CHIP='1'
-# Define button and PIR sensor pins as an input
-chip = gpiod.Chip(CHIP)
-getlines = chip.get_lines(getoffsets)
-getlines.request(consumer="app5 in", type=gpiod.LINE_REQ_DIR_IN)
-# Define led pins as output
-setlines = chip.get_lines(setoffests)
-setlines.request(consumer="app5 out", type=gpiod.LINE_REQ_DIR_OUT)
-# turn leds OFF 
-setlines.set_values([0])
+Files = [open(MAX0,"r"),open(MAX1,"r"),open(MAX2,"r")]
+
+temp = [0,0,0]
+
+def readValues():
+    temps = []
+    for file in Files:
+        file.seek(0)
+        data = file_name.read()[:-1]
+        temps.append(int(data))
+    temp = temps
+    return temps
+
+
+        
 
 @app.route("/")
 def index():
-	# Read GPIO Status
-	vals = getlines.get_values()
-	templateData = {
-	 	'button'  : getlines.get_values()[0],
-  		'ledRed'  : setlines.get_values()[0]
+    # Read GPIO Status
+    vals = readValues()
+    templateData = {
+     	'temp1'  : vals[0],
+        'temp2'  : vals[1],
+        'temp3'  : vals[2]
     }
-	return render_template('index5.html', **templateData)
+    return render_template('index5.html', **templateData)
 	
 @app.route("/<deviceName>/<action>")
 def action(deviceName, action):
-	if action == "on":
-		setlines.set_values([1])
-	if action == "off":
-		setlines.set_values([0])
-	if action == "toggle":
-		setlines.set_values([not setlines.get_values()[0]])
-
-	templateData = {
-	 	'button'  : getlines.get_values()[0],
-  		'ledRed'  : setlines.get_values()[0]
-	}
-	return render_template('index5.html', **templateData)
+    tempVal = temp
+    if action == "read":
+        tempVal = readValues() 
+    templateData = {
+     	'temp1'  : tempVal[0],
+        'temp2'  : tempVal[1],
+        'temp3'  : tempVal[2]
+    }
+    return render_template('index5.html', **templateData)
 if __name__ == "__main__":
    app.run(host='0.0.0.0', port=8081, debug=True)
